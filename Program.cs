f154706aaf4e821787f2741282f206b11b067206
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-02 18:25:32 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _21.OopEncapsulationUsingPropertiesGetSet
+{
+    public class Bank
+    {
+        private double balance; // field variable declared
+        public double Balance
+        {
+            get { return balance; }
+
+            set
+            {  //validate the value
+                if (value < 0)
+                {
+                    Console.WriteLine("value can not be negative");
+                }
+                balance = value;
+            }
+        }
+
+    }
+
+    class BankUser
+    {
+        public static void Main()
+        {
+            Bank SBI = new Bank();
+            SBI.Balance = 100;   //settign the value = 100 using set property.
+            Console.WriteLine(SBI.Balance);// accessing the value of the Object SBT using get property
+            SBI.Balance = -50; // here checking the value using set property
+            Console.WriteLine(SBI.Balance);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
